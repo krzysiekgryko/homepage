0118b3bbe2926fe3fd604c17a5885921c3c7931a
Krzysiek Gryko <k.gryko@gmail.com>
2019-06-16 20:43:50 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+alert ('Patrzymy kulturalnie')
+console.log('Patrzymy kulturalnie');
+
+const name = 'Krzysiek';
+const age = 39;
+console.log(name);
+console.log(age);
+
+console.log(`Nie jestem obcy 😱`)
+
+console.log(`Jestem ${name} i mam ${age} lat`);
+
